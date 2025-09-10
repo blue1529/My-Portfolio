6daa0882bc14bfd7b4122b4c77ae3c72f488ad33
Newton Madalitso Kalulu <newtonkalulu00@gmail.com>
2025-09-10 05:14:29 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  toggle.checked = true;
+}
+
+toggle.addEventListener("change", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+
+//header dissapearing
+let lastScrollY = window.scrollY;
+    const header = document.getElementById("header");
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > lastScrollY) {
+        // scrolling down
+        header.classList.add("hidden");
+      } else {
+        // scrolling up
+        header.classList.remove("hidden");
+      }
+      lastScrollY = window.scrollY;
+    });
